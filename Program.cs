e76fe3adcc67a349230742f5db9ad643d34efb23
ahamedsahib <adsahib39@gmail.com>
2021-07-10 18:25:54 +0530

    [Ahamed]Inserting Element at End

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Program");
+            LinkedList linkedList = new LinkedList();
+            //Adding Elements to linked list
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+
+            linkedList.Display();
         }
     }
 }
